d5a07ad09f8eb2dea75ef9772130a4e74027ffd3
Finsenty54 <fin12138@gmail.com>
2021-03-01 20:06:33 +0800

    add reg memo

--- debuger.cpp
+++ debuger.cpp
@@ -2,8 +2,11 @@
 利用ptrace来跟踪，继续执行，读取和写入内存
 PTRACE_ATTACHME
 PTRACE_CONT
-PTRACE_PEEKDATA
-PTRACE_POKEDATA
+PTRACE_PEEKDATA 读取
+PTRACE_POKEDATA 写入
+PTRACE_GETREGS 获得寄存器值
+PTRACE_SETREGS 写入寄存器值
+PTRACE_SINGLESTEP
 
 并搭配waitpid()，收到信号后，唤醒debuger
 64位下word是64位
@@ -25,11 +28,116 @@ stol()将str转换为数字
 #include <string.h>
 #include <sstream>
 
+#include <algorithm>
+#include <sys/user.h>
+#include <iomanip>
+
 #include "linenoise/linenoise.h"
 
 using namespace std;
 
-void error(char *s)
+enum class reg{
+	rax,rbx,rcx,rdx,
+	rdi,rsi,rbp,rsp,
+	r8,r9,r10,r11,
+	r12,r13,r14,r15,
+	rip,rflags,cs,
+	orig_rax,fs_base,
+	gs_base,
+	fs,gs,ss,ds,es
+};
+
+constexpr std::size_t n_registers=27;
+
+struct reg_descriptor
+{
+	reg r;
+	int dwarf_r;
+	std::string name;
+};
+
+const array<reg_descriptor,n_registers> g_register_descriptors{{
+	{ reg::r15,15,"r15"},
+	{ reg::r14,14,"r14"},
+	{ reg::r13, 13, "r13" },
+    { reg::r12, 12, "r12" },
+    { reg::rbp, 6, "rbp" },
+    { reg::rbx, 3, "rbx" },
+    { reg::r11, 11, "r11" },
+    { reg::r10, 10, "r10" },
+    { reg::r9, 9, "r9" },
+    { reg::r8, 8, "r8" },
+    { reg::rax, 0, "rax" },
+    { reg::rcx, 2, "rcx" },
+    { reg::rdx, 1, "rdx" },
+    { reg::rsi, 4, "rsi" },
+    { reg::rdi, 5, "rdi" },
+    { reg::orig_rax, -1, "orig_rax" },
+    { reg::rip, -1, "rip" },
+    { reg::cs, 51, "cs" },
+    { reg::rflags, 49, "eflags" },
+    { reg::rsp, 7, "rsp" },
+    { reg::ss, 52, "ss" },
+    { reg::fs_base, 58, "fs_base" },
+    { reg::gs_base, 59, "gs_base" },
+    { reg::ds, 53, "ds" },
+    { reg::es, 50, "es" },
+    { reg::fs, 54, "fs" },
+    { reg::gs, 55, "gs" },
+}};
+
+uint64_t get_register_value(pid_t pid, reg r)
+{
+	user_regs_struct regs;
+	ptrace(PTRACE_GETREGS,pid,nullptr,&regs);
+
+	//lambda()匿名函数，[]捕获外部变量，()形参列表，g_register_descriptors与r比较，因为传进来的是r，然后des与regs结构相同，可以查找
+	auto it=find_if(begin(g_register_descriptors),end(g_register_descriptors),
+	[r](auto&& rd) {return rd.r==r;});
+
+	//re...cast类型转换，地址相减，取内容，获得寄存器值
+	return *(reinterpret_cast<uint64_t*>(&regs)+(it-begin(g_register_descriptors)));
+}
+
+void set_register_value(pid_t pid,reg r,uint64_t value)
+{
+	user_regs_struct regs;
+	ptrace(PTRACE_GETREGS,pid, nullptr, &regs);
+	auto it=find_if(begin(g_register_descriptors),end(g_register_descriptors),
+	[r](auto&& rd){return rd.r==r;});
+
+	*(reinterpret_cast<uint64_t*>(&regs)+(it-begin(g_register_descriptors)))=value;
+	ptrace(PTRACE_SETREGS,pid,nullptr,&regs);
+}
+
+uint64_t get_register_value_from_dwarf_register(pid_t pid,unsigned regnum)
+{
+	auto it = find_if(begin(g_register_descriptors),end(g_register_descriptors),
+	[regnum](auto&& rd){return rd.dwarf_r==regnum;});
+	if (it==end(g_register_descriptors))
+	{
+		throw out_of_range{"Unknown dwarf register!"};
+	}
+	return get_register_value(pid,it->r);	
+}
+
+string get_register_name(reg r)
+{
+	auto it =find_if(begin(g_register_descriptors),end(g_register_descriptors),
+	[r](auto&& rd){return rd.r==r;});
+
+	return it->name;
+}
+
+reg get_register_from_name(const string& name)
+{
+	auto it=find_if(begin(g_register_descriptors),end(g_register_descriptors),
+	[name](auto&& rd){return rd.name==name;});
+
+	return it->r;
+}
+
+void error(string s)
 {
 	cerr<<s<<" failed!\n";
 	cerr<<strerror(errno);
@@ -65,6 +173,10 @@ class breakpoint
 		breakpoint(pid_t pid,intptr_t addr)
 			:m_pid(pid),m_addr{addr},m_enabled{false},m_saved_data{}
 		{}
+
+		//遇到错误/usr/include/c++/10/tuple:1689:70: error: 
+		//no matching function for call to ‘breakpoint::breakpoint()’
+		breakpoint(){}
 
 		void enable();
 		void disable();
@@ -122,6 +234,13 @@ public:
 	void handle_command(const string &);
 	void continue_execution();
 	void set_breakpoint_at_address(intptr_t addr);
+	void dump_registers();
+	uint64_t read_memory(uint64_t address);
+	void write_memory(uint64_t address,uint64_t value);
+	uint64_t get_pc();
+	void set_pc(uint64_t);
+	void step_over_breakpoint();
+	void wait_for_signal();
 };
 
 debuger::debuger(string prog_name, pid_t pid):m_prog_name{move(prog_name)},m_pid{pid}{}
@@ -165,6 +284,36 @@ void debuger::handle_command(const string & line)
 		//(const string&  str, size_t* idx = 0, int base = 10);
 		set_breakpoint_at_address(stol(addr,0,16));
 	}
+	else if (is_prefix(command,"register"))
+	{
+		if (is_prefix(args[1],"dump"))
+		{
+			dump_registers();
+		}
+		else if (is_prefix(args[1],"read"))
+		{
+			cout<<get_register_value(m_pid,get_register_from_name(args[2]))<<endl;
+		}
+		else if (is_prefix(args[1],"write"))
+		{
+			string val(args[3],2);
+			set_register_value(m_pid,get_register_from_name(args[2]),stol(val,0,16));
+		}
+	}
+	else if (is_prefix(command,"memory"))
+	{
+		string addr{args[2],2}; //assume 0xaddress
+
+		if (is_prefix(args[1],"read"))
+		{
+			cout<<hex<<read_memory(stol(addr,0,16))<<endl;
+		}
+		if (is_prefix(args[1],"write"))
+		{/* condition */
+			string val{args[3],2};
+			write_memory(stol(addr,0,16),stol(val,0,16));
+		}	
+	}
 	else
 	{
 		cerr<<"Unknown command\n";
@@ -173,11 +322,9 @@ void debuger::handle_command(const string & line)
 
 void debuger::continue_execution()
 {
+	step_over_breakpoint();
 	ptrace(PTRACE_CONT,m_pid,nullptr,nullptr);
-
-	int wait_status;
-	auto options=0;
-	waitpid(m_pid,&wait_status,options);
+	wait_for_signal();
 }
 
 void debuger::set_breakpoint_at_address(intptr_t addr)
@@ -190,6 +337,64 @@ void debuger::set_breakpoint_at_address(intptr_t addr)
 	m_breakpoints.insert(make_pair(addr,bp));
 }
 
+void debuger::dump_registers()
+{
+	for (const auto& rd : g_register_descriptors) //c++11 中带入
+	{
+		cout<<rd.name<<" 0x"
+		<<setfill('0')<<setw(16)<<hex<<get_register_value(m_pid,rd.r)<<endl;
+		//设置std::setw将填充什么样的字符，如:std::setfill('*')
+	}
+	
+}
+
+uint64_t debuger::read_memory(uint64_t address)
+{
+	return ptrace(PTRACE_PEEKDATA,m_pid,address,nullptr);
+}
+
+void debuger::write_memory(uint64_t address,uint64_t value)
+{
+	ptrace(PTRACE_POKEDATA,m_pid,address,value);
+}
+
+void debuger::step_over_breakpoint()
+{
+	//rip 此时为断点值加一
+	auto possible_breakpoint_location = get_pc()-1;
+	if (m_breakpoints.count(possible_breakpoint_location))
+	{
+		auto& bp=m_breakpoints[possible_breakpoint_location];
+
+		if (bp.is_enabled())
+		{
+			auto previous_instruction_address =possible_breakpoint_location;
+			set_pc(previous_instruction_address);
+
+			bp.disable();
+			ptrace(PTRACE_SINGLESTEP,m_pid,nullptr,nullptr);
+			wait_for_signal();
+			bp.enable();
+		}	
+	}
+}
+
+uint64_t debuger::get_pc()
+{
+	return get_register_value(m_pid,reg::rip);
+}
+
+void debuger::set_pc(uint64_t pc)
+{
+	set_register_value(m_pid,reg::rip,pc);
+}
+
+void debuger::wait_for_signal()
+{
+	int wait_status;
+	auto options=0;
+	waitpid(m_pid,&wait_status,options);
+}
 
 int main(int argc, char *argv[])
 {
